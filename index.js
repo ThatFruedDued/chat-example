22320371614b4bdb11c9ddaa76d2baf6104d22ae
ThatFruedDued <57722939+ThatFruedDued@users.noreply.github.com>
2019-12-18 17:50:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('newmsg', function(msg, socketid){
+    io.emit('chat message', socketid);
   });
 });
 
