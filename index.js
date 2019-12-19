92b5719380f5e75eb0c8afe838c665b482c3c29e
ThatFruedDued <57722939+ThatFruedDued@users.noreply.github.com>
2019-12-19 11:01:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('newmsg', function(msg, pid){
+    io.emit('newmsg', '<a style="color:red>"' + guser(pid).rank + '</a> ' + guser(pid).name + ': ' + msg);
+  });
+  socket.on('original-setup', function(name){
+    var newpid = Math.floor(Math.random() * 100000000000000000);
+    wuser(new user(newpid, socket.id, name, '0', ''), newpid);
+    io.to(socket.id).emit('newpid', newpid);
+  });
+  socket.on('regular-setup', function(pid){
+    wuser('same', socket.id, 'same', 'same', 'same');
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
 function user(pid, tid, name, muted, rank){
   this.pid = pid;
   this.tid = tid;
@@ -34,25 +56,3 @@ function wuser(user, pid){
 function guser(pid){
   return JSON.parse(localStorage.getItem('user-' + pid))
 }
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('newmsg', function(msg, pid){
-    io.emit('newmsg', '<a style="color:red>"' + guser(pid).rank + '</a> ' + guser(pid).name + ': ' + msg);
-  });
-  socket.on('original-setup', function(name){
-    var newpid = Math.floor(Math.random() * 100000000000000000);
-    wuser(new user(newpid, socket.id, name, '0', ''), newpid);
-    io.to(socket.id).emit('newpid', newpid);
-  });
-  socket.on('regular-setup', function(pid){
-    wuser('same', socket.id, 'same', 'same', 'same');
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
